1150c8ec289cff5aecd3bbc893b23bca50ebb60b
Chris Ross <chriseross@gmail.com>
2016-12-21 02:03:54 +0000

    finishes up first stage of landing page, creates colors topic page, and fixes some issues with practicing colors page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  root 'home_pages#index'
  resources :topics, only: [:index, :show]
+ get 'practice_colors', to: 'home_pages#practice_colors'
+ get 'learn_colors', to: 'home_pages#learn_colors'
  get 'colors', to: 'home_pages#colors'
 end
