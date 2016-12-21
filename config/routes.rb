2fdc01bef02304c567bba8dbbfebdee415c1a507
Chris Ross <chriseross@gmail.com>
2016-12-21 21:24:36 +0000

    adds number practice

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
  get 'practice_colors', to: 'home_pages#practice_colors'
  get 'learn_colors', to: 'home_pages#learn_colors'
  get 'colors', to: 'home_pages#colors'
+ get 'numbers', to: 'home_pages#numbers'
+ get 'practice_numbers', to: 'home_pages#practice_numbers'
+ get 'learn_numbers', to: 'home_pages#learn_numbers'
+
 end
